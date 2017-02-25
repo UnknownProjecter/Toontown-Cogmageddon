48dbe5009c0d917b9316605e20a7c1fb1ea43b7d
leotheepicgamer <leotz58@gmail.com>
2017-02-25 09:56:09 -0500

    Created DistributedToon.py needs work

---
+++ toontown/toon/DistributedToon.py
@@ -0,0 +1,4 @@
+from direct.distributed.DistributedSmoothNode import DistributedSmoothNode
+from toontown.avatar.DistributedAvatar import DistributedAvatar
+from toontown.toon.Toon import Toon
+from toontown.toon.ToonDNA import ToonDNA
